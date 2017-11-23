50b5fa01d82cfad711b60e6ec63b566bcea07492
a32kita <a32kita@yahoo.co.jp>
2017-11-23 23:46:40 +0900

    WPFテスト用コード追加、正常に動作しません

---
+++ GuiTestWPF.Net45/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+using LocusCommon.Windows.ViewModels;
+
+namespace GuiTestWPF.Net45
+{
+    /// <summary>
+    /// MainWindow のビューモデルを定義します。
+    /// </summary>
+    public class MainWindowViewModel : ExtendedViewModelBase
+    {
+        // 非公開フィールド
+
+        private DelegateCommand alphaTestBlockButtonCommand;
+
+
+        // 公開プロパティ :: バインディングプロパティ
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool AlphaTestBlockCheckBoxChecked
+        {
+            get => this.GetBindingValue<bool>(nameof(this.AlphaTestBlockCheckBoxChecked));
+            set => this.SetBindingValue(nameof(this.AlphaTestBlockCheckBoxChecked), value);
+        }
+
+
+        // 公開プロパティ :: コマンド
+
+        /// <summary>
+        /// AlphaTestBlock の Button 用のコマンドを取得します。
+        /// </summary>
+        public ICommand AlphaTestBlockButtonCommand
+        {
+            get => this.alphaTestBlockButtonCommand;
+        }
+
+
+        // コンストラクタ
+
+        /// <summary>
+        /// 新しい MainWindowViewModel クラスのインスタンスを初期化します。
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            var alphaTestBlockButtonCommandParam = new DelegateCommandInitializeParameter()
+            {
+                Command = param => MessageBox.Show(""),
+                CanExecute = param => this.AlphaTestBlockCheckBoxChecked
+            };
+
+            this.alphaTestBlockButtonCommand = new DelegateCommand(alphaTestBlockButtonCommandParam);
+        }
+    }
+}
---
+++ LocusCommon/Windows/ViewModels/DelegateCommandInitializeParameter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocusCommon.Windows.ViewModels
+{
+    /// <summary>
+    /// DelegateCommand の初期化用パラメータクラスを提供します。
+    /// </summary>
+    public class DelegateCommandInitializeParameter
+    {
+        // 非公開フィールド
+        private Action<Object> command;
+        private Func<Object, bool> canExecute;
+
+
+        // 公開プロパティ
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Action<Object> Command
+        {
+            get => this.command;
+            set => this.command = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Func<Object, bool> CanExecute
+        {
+            get => this.canExecute;
+            set => this.canExecute = value;
+        }
+
+
+        // コンストラクタ
+
+        /// <summary>
+        /// 新しい DelegateCommandInitializeParameter クラスのインスタンスを初期化します。
+        /// </summary>
+        public DelegateCommandInitializeParameter()
+        {
+            this.command = null;
+            this.canExecute = null;
+        }
+    }
+}
