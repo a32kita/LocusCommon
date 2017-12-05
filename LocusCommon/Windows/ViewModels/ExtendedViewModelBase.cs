ce68a71544a269e05b6afed14cb9b3cbefc32e87
a32kita <a32kita@yahoo.co.jp>
2017-12-06 00:30:08 +0900

    CallerMemberName属性を使用して、ExtendedViewModelBase でプロパティ名を省略した値の読み書きが可能になりました。

--- LocusCommon/Windows/ViewModels/ExtendedViewModelBase.cs
+++ LocusCommon/Windows/ViewModels/ExtendedViewModelBase.cs
@@ -7,8 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Input;
 
+
 namespace LocusCommon.Windows.ViewModels
 {
+#if NET45||NET461
+    using CallerMemberNameAttribute = System.Runtime.CompilerServices.CallerMemberNameAttribute;
+#endif
+
     /// <summary>
     /// このアプリケーションで使用するViewModelの基礎となるクラスを提供します。
     /// このクラスは継承専用で、直接のインスタンス化は出来ません。
@@ -31,16 +36,22 @@ namespace LocusCommon.Windows.ViewModels
 
 
         // 限定公開メソッド
-
+        
+#if NET45||NET461
+        /// <summary>
+        /// プロパティ辞書からプロパティの値を取得します。
+        /// </summary>
+        /// <typeparam name="T">プロパティの型</typeparam>
+        /// <param name="propertyName">バインディングプロパティ名。プロパティの中で呼び出しを行っている場合、省略が可能です。</param>
+        /// <returns></returns>
+        protected T GetBindingValue<T>([CallerMemberName] string propertyName = null)
+#else
         /// <summary>
         /// プロパティ辞書からプロパティの値を取得します。
         /// </summary>
         /// <typeparam name="T">プロパティの型</typeparam>
         /// <param name="propertyName">バインディングプロパティ名</param>
         /// <returns></returns>
-#if NET45||NET461
-        protected T GetBindingValue<T>(string propertyName)
-#else
         protected T GetBindingValue<T>(string propertyName)
 #endif
         {
@@ -71,6 +82,18 @@ namespace LocusCommon.Windows.ViewModels
             this.propertyValues[propertyName] = value;
             this.RaisePropertyChanged(propertyName);
         }
+
+#if NET45 || NET461
+        /// <summary>
+        /// プロパティ辞書へプロパティの値を記録し、RaisePropertyChangedを実行します。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="propertyName">このパラメータは使用されません。</param>
+        protected void SetBindingValue(Object value, [Browsable(false)][CallerMemberName] string propertyName = null)
+        {
+            this.SetBindingValue(propertyName, value);
+        }
+#endif
     }
 }
 
