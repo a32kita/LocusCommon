6d9e5f19d08940c37209905fed4fb9407fc27266
a32kita <a32kita@yahoo.co.jp>
2017-12-25 03:45:26 +0900

    あ

--- LocusCommon.Serialization/Ini/IniSectionConverter.cs
+++ LocusCommon.Serialization/Ini/IniSectionConverter.cs
@@ -7,39 +7,15 @@ namespace LocusCommon.Serialization.Ini
     /// <summary>
     /// 
     /// </summary>
-    public abstract class IniSectionConverter<TValueType>
+    public abstract class IniSectionConverter
     {
         // プロパティ
-        
-        /// <summary>
-        /// このコンバーターがサポートする対象の型を取得します。
-        /// </summary>
-        public Type ValueType
-        {
-            get => typeof(TValueType);
-        }
-
-        /// <summary>
-        /// このコンバータが INI データの読み込みをサポートしているかどうかを示す値を取得します。
-        /// </summary>
-        public abstract bool CanRead
-        {
-            get;
-        }
-
-        /// <summary>
-        /// このコンバータが INI データの書き出しをサポートしているかどうかを示す値を取得します。
-        /// </summary>
-        public abstract bool CanWrite
-        {
-            get;
-        }
 
 
         // コンストラクタ
 
         /// <summary>
-        /// 新しい <see cref="IniSectionConverter{TValueType}"/> クラスのインスタンスを初期化します。
+        /// 新しい <see cref="IniSectionConverter"/> クラスのインスタンスを初期化します。
         /// </summary>
         public IniSectionConverter()
         {
@@ -50,17 +26,17 @@ namespace LocusCommon.Serialization.Ini
         // 公開メソッド
 
         /// <summary>
-        /// INIデータ へ <see cref="TValueType"/> を変換する機能を実装します。
+        /// INIデータへ任意の型のインスタンスを変換する機能を実装します。
         /// </summary>
         /// <param name="section"></param>
         /// <param name="target"></param>
-        public abstract void WriteIni(IniSection section, TValueType target);
+        public abstract void WriteIni(IniSection section, Object target);
 
         /// <summary>
-        /// INIデータ を <see cref="TValueType"/> へ変換する機能を実装します。
+        /// INIデータを任意の型のインスタンスへ変換する機能を実装します。
         /// </summary>
         /// <param name="section"></param>
         /// <returns></returns>
-        public abstract TValueType ReadIni(IniSection section);
+        public abstract Object ReadIni(IniSection section);
     }
 }
