29bfff6f79e1ce59d2f5d68a94949317b594f1c2
a32kita <a32kita@yahoo.co.jp>
2018-02-04 05:24:40 +0900

    oyasumi

--- LocusCommon/Windows/LocusWindow.cs
+++ LocusCommon/Windows/LocusWindow.cs
@@ -19,7 +19,30 @@ namespace LocusCommon.Windows
         // 公開プロパティ
 
 
+        // 依存関係プロパティ
+        
+        public bool DoClose
+        {
+            get { return (bool)GetValue(DoCloseProperty); }
+            set { SetValue(DoCloseProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DoClose.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DoCloseProperty =
+            DependencyProperty.Register("DoClose", typeof(bool), typeof(LocusWindow), new PropertyMetadata(false, (d, e) =>
+            {
+            }));
+
+        
         // コンストラクタ
+
+        /// <summary>
+        /// 新しい <see cref="LocusWindow"/> クラスのインスタンスを初期化します。
+        /// </summary>
+        public LocusWindow()
+        {
+            
+        }
 
 
         // 非公開メソッド
