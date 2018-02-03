2a3672e7bd43f51daccddd4c3545b3352b552579
a32kita <a32kita@yahoo.co.jp>
2018-02-04 05:19:54 +0900

    test

---
+++ LocusCommon/Windows/LocusWindow.cs
@@ -0,0 +1,29 @@
+﻿#if NET35||NET40||NET45||NET461
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace LocusCommon.Windows
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LocusWindow : Window
+    {
+        // 非公開フィールド
+
+
+
+        // 公開プロパティ
+
+
+        // コンストラクタ
+
+
+        // 非公開メソッド
+    }
+}
+
+#endif
